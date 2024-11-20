f2abcd7a44e26e8e51a7e27b87a791c3684f0dc6
Daniel Trugman <dtrugman@gmail.com>
2024-11-20 16:09:37 +0000

    Allow setting the base_url in the singleton sample

--- samples/openai/client_singleton.py
+++ samples/openai/client_singleton.py
@@ -3,4 +3,5 @@ import os
 from requestyai import AInsights
 
 requesty_key = os.environ["REQUESTY_KEY"]
-insights = AInsights.new_client(api_key=requesty_key)
+base_url = os.environ.get("REQUESTY_BASE_URL", default=None)
+insights = AInsights.new_client(api_key=requesty_key, base_url=base_url)
